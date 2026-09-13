f6607b39321eb31693601f2fdd28da1a78b7345c
Purva Yadkikar <Purva7@users.noreply.github.com>
2026-09-13 10:02:44 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+  
+        String c = s.replaceAll("[^a-zA-Z0-9]", "");
+        int i = 0;
+        int j = c.length() - 1;
+
+        while (i < j) {
+            char left = Character.toLowerCase(c.charAt(i));
+            char right = Character.toLowerCase(c.charAt(j));
+
+            if (left != right) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
